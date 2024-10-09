9319e792e84c529188e90789bcd63945b95e8e82
richard <richard.eagle1992@gmail.com>
2024-10-09 10:51:49 -0700

    feat: complete header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+import "./Header.css";
+
 const Header: React.FC = () => {
-  return <header>Header</header>;
+  return (
+    <header className="app-header" role="banner">
+     
+      <h1 id="main-title">Cribl Log Viewer</h1>
+      
+      <p aria-describedby="main-title">
+        Streaming logs directly from the server
+      </p>
+    </header>
+  );
 };
 
 export default Header;
