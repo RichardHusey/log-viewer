4c0f9ce927f0905f158b44cd874decdb19beb3c0
richard <richard.eagle1992@gmail.com>
2024-10-09 10:42:34 -0700

    feat: implement ContextAPI - LogContext

--- src/pages/LogViewer.tsx
+++ src/pages/LogViewer.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Timeline from "../components/Timeline/Timeline";
 import LogTable from "../components/LogTable/LogTable";
+import { useLogs } from "../context/LogContext"; // Import LogContext with correct types
 
 const LogViewer: React.FC = () => {
+  const { error } = useLogs();
 
   return (
     <div className="log-viewer">
       <Header />
-      <Timeline/>
+      {error && <p className="error-message">{error}</p>}
+      <Timeline />
       <LogTable />
     </div>
   );
